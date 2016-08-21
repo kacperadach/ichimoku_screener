59e0a722f83f4e96cb620f50e498ff1311616156
Kacper Adach <kacperadach@gmail.com>
2016-08-21 16:32:14 -0400

    made filter setting parameters into env vars

--- GetStockData.py
+++ GetStockData.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from os import getenv
 
 from yahoo_finance import Share
 
@@ -39,11 +40,11 @@ def filter_stocks(s, data):
     try:
         if not data:
             return True
-        elif float(data[0]['Close']) <= 1 or \
+        elif not _over_min_stock_value(data[0]['Close']) or \
             not _over_market_cap_filter(s.get_market_cap()) or \
             not _over_pe_filter(s.get_price_earnings_ratio()) or \
             not s.get_avg_daily_volume() or \
-            float(s.get_avg_daily_volume()) < 400000 or \
+            not _over_volume_filter(s.get_avg_daily_volume()) or \
             (float(s.get_year_high()) * .99) <= float(data[0]['High']):
             return True
         else:
@@ -51,14 +52,35 @@ def filter_stocks(s, data):
     except:
         return True
 
+def _over_min_stock_value(last_close):
+    try:
+        return float(last_close) >= getenv('MIN_STOCK_VALUE', 1)
+    except:
+        return False
+
+def _over_volume_filter(volume):
+    try:
+        return float(volume) >= getenv('VOLUME_MIN', 400000)
+    except:
+        return False
+
 def _over_market_cap_filter(market_cap_string):
     try:
-        return market_cap_string[-1] == 'B'
-    except TypeError:
+        return _parse_market_cap_string(market_cap_string) >= getenv('MARKET_CAP_MIN', 1000000000)
+    except:
         return False
 
 def _over_pe_filter(pe_string):
     try:
-        return float(pe_string) < 50 and float(pe_string) > 0
+        return float(pe_string) < getenv('PE_MAX', 50) and float(pe_string) > getenv('PE_MIN', 0)
     except:
         return False
+
+def _parse_market_cap_string(market_cap_string):
+    if market_cap_string[-1].upper() == 'B':
+        return 1000000000 * float(market_cap_string[0:-1])
+    elif market_cap_string[-1].upper() == 'M':
+        return 1000000 * float(market_cap_string[0:-1])
+    else:
+        logger.info("Market cap not in millions or billions, {}".format(market_cap_string))
+        return 0
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from os import getenv
 
 from AnalyseIchimoku import analyze_ichimoku
 from EmailSender import send_email
@@ -10,10 +11,20 @@ def run():
     if datetime.today().weekday() not in (4, 5):
         logger.info("Starting ichimoku screener")
         start_time = datetime.now()
+        logger.info("Filter Settings: {}".format(_get_filter_settings()))
         ichi_dict = (analyze_ichimoku())
         send_email(ichi_dict)
         end_time = datetime.now()
         logger.info("Finished screening. Total time: {}".format(end_time - start_time))
+
+def _get_filter_settings():
+    return {
+        "min_stock_value": getenv('MIN_STOCK_VALUE', 1),
+        "volume_min": getenv('VOLUME_MIN', 400000),
+        "market_cap_min": getenv('MARKET_CAP_MIN', 1000000000),
+        "pe_max": getenv('PE_MAX', 50),
+        "pe_min": getenv('PE_MIN', 0)
+    }
 
 if __name__ == "__main__":
     run()
